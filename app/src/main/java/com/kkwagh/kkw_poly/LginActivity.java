3d7a673dd9588b94a4a47ebb26743de46fdf0c57
senor-7r4c3r <42082316+senor-7r4c3r@users.noreply.github.com>
2020-05-02 15:20:30 +0530

    Renamed the MainActivity to LoginActivity

--- app/src/main/java/com/kkwagh/kkw_poly/LginActivity.java
+++ app/src/main/java/com/kkwagh/kkw_poly/LginActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //test
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
