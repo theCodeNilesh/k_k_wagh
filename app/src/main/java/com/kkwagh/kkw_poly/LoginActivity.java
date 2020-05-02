4b5e20acb0d4f2a03f70d28a04a2e0c67d147b16
Mohit-Chordiya <57399811+Mohit-Chordiya@users.noreply.github.com>
2020-05-02 15:31:46 +0530

    Testing if git is working

--- app/src/main/java/com/kkwagh/kkw_poly/LoginActivity.java
+++ app/src/main/java/com/kkwagh/kkw_poly/LoginActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
