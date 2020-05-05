e9e4120212f0dcacd93e2fe39469251593b56ff5
theCodeNilesh <nilesh121wadile@gmail.com>
2020-05-05 13:39:27 +0530

    Registration UI is Added

--- app/src/main/java/com/kkwagh/kkw_poly/RegistrationActivity.java
+++ app/src/main/java/com/kkwagh/kkw_poly/RegistrationActivity.java
@@ -1,13 +1,53 @@
 package com.kkwagh.kkw_poly;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatRadioButton;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    AppCompatRadioButton rbleft, rbright;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        rbleft = findViewById(R.id.rbleft);
+        rbright = findViewById(R.id.rbright);
+
+
+
     }
+
+
+    public void onRadioButtonClicked(View view){
+        boolean isSelected = ((AppCompatRadioButton)view).isChecked();
+        switch (view.getId()){
+
+            case R.id.rbleft:
+                if (isSelected){
+                    rbleft.setTextColor(Color.WHITE);
+                    rbright.setTextColor(Color.BLACK);
+                }
+
+                break;
+            case R.id.rbright:
+
+                if (isSelected){
+                    rbright.setTextColor(Color.WHITE);
+                    rbleft.setTextColor(Color.BLACK);
+
+                }
+                break;
+
+        }
+
+
+    }
+
 }
--- app/src/main/java/com/kkwagh/kkw_poly/VerifyOTPActivity.java
+++ app/src/main/java/com/kkwagh/kkw_poly/VerifyOTPActivity.java
@@ -1,6 +1,9 @@
 package com.kkwagh.kkw_poly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,19 @@ public class VerifyOTPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_otp);
+
+        Button send_otp;
+
+        send_otp = (Button) findViewById(R.id.send_otp) ;
+
+
+        send_otp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(VerifyOTPActivity.this, RegistrationActivity.class));
+            }
+        });
+
 
 
     }
