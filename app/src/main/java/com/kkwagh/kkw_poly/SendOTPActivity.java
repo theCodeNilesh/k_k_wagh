8bbf894346d7ef2b1e807118ccca31dbed36ffce
Mohit-Chordiya <57399811+Mohit-Chordiya@users.noreply.github.com>
2020-05-05 16:03:28 +0530

    Send OTP and Verify OTP - Work in progress

--- app/src/main/java/com/kkwagh/kkw_poly/SendOTPActivity.java
+++ app/src/main/java/com/kkwagh/kkw_poly/SendOTPActivity.java
@@ -1,32 +1,79 @@
 package com.kkwagh.kkw_poly;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
 public class SendOTPActivity extends AppCompatActivity {
+    ImageView top_circle, circle_top;
+    EditText phone_no;
+    String PhoneNoHolder;
+    Button send_otp;
+    String finalResult;
+    Boolean CheckEditText;
+    String HttpURL = "http://192.168.43.238/KKWP/kkwp-app-backend/sendOTP_api.php";
+    HashMap<String, String> hashMap = new HashMap<>();
+    HttpParser httpParse = new HttpParser();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_otp);
+        send_otp = findViewById(R.id.send_otp);
 
-        ImageView top_circle, circle_top;
-        Button send_opt;
-
-        send_opt = (Button) findViewById(R.id.send_otp);
-
-        send_opt.setOnClickListener(new View.OnClickListener() {
+        send_otp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SendOTPActivity.this, VerifyOTPActivity.class));
+                CheckEditTextIsEmptyOrNot();
+                if (CheckEditText) {
+                    OTPFunction(PhoneNoHolder);
+                } else {
+                    Toast.makeText(SendOTPActivity.this, "Please fill all form fields", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
 
+    public void CheckEditTextIsEmptyOrNot() {
+        PhoneNoHolder = phone_no.getText().toString();
 
+        if (TextUtils.isEmpty(PhoneNoHolder)) {
+            CheckEditText = false;
+        } else {
+            CheckEditText = true;
+        }
+    }
 
+    public void OTPFunction(final String mobile_no) {
+        class OTPFunctionClass extends AsyncTask<String, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+                super.onPostExecute(httpResponseMsg);
+                Toast.makeText(SendOTPActivity.this, httpResponseMsg, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                hashMap.put("mobile_no", params[0]);
+                finalResult = httpParse.postRequest(hashMap, HttpURL);
+                return finalResult;
+            }
+        }
+        OTPFunctionClass userRegisterFunctionClass = new OTPFunctionClass();
+        userRegisterFunctionClass.execute(mobile_no);
     }
 }
--- app/src/main/java/com/kkwagh/kkw_poly/VerifyOTPActivity.java
+++ app/src/main/java/com/kkwagh/kkw_poly/VerifyOTPActivity.java
@@ -4,28 +4,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class VerifyOTPActivity extends AppCompatActivity {
+    Button verify_otp;
+    EditText digit1, digit2, digit3, digit4;
+    String appended_otp, digit1Holder, digit2Holder, digit3Holder, digit4Holder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_otp);
+        verify_otp = findViewById(R.id.verify_otp);
 
-        Button send_otp;
+        digit1 = findViewById(R.id.otp1);
+        digit2 = findViewById(R.id.otp2);
+        digit3 = findViewById(R.id.otp3);
+        digit4 = findViewById(R.id.otp4);
 
-        send_otp = (Button) findViewById(R.id.send_otp) ;
+        digit1Holder = digit1.getText().toString();
+        digit2Holder = digit2.getText().toString();
+        digit3Holder = digit3.getText().toString();
+        digit4Holder = digit4.getText().toString();
 
+        appended_otp = digit1Holder + digit2Holder + digit3Holder + digit4Holder;
 
-        send_otp.setOnClickListener(new View.OnClickListener() {
+        Intent intent = getIntent();
+        final String phone_no = intent.getStringExtra("phone_no");
+        final String from_api_otp = intent.getStringExtra("otp");
+
+        verify_otp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(VerifyOTPActivity.this, RegistrationActivity.class));
+                if (from_api_otp == appended_otp) {
+                    Intent intent5 = new Intent(VerifyOTPActivity.this, RegistrationActivity.class);
+                    intent5.putExtra("phone_no", phone_no);
+                    startActivity(intent5);
+                } else {
+                    Toast.makeText(VerifyOTPActivity.this, "OTP is not valid", Toast.LENGTH_LONG).show();
+                }
             }
         });
-
-
-
     }
 }
