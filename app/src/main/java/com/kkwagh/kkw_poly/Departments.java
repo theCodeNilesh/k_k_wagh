81d23e157fb3196330cdf78c7f4fa3e778c17239
theCodeNilesh <61627365+theCodeNilesh@users.noreply.github.com>
2020-05-28 16:23:40 +0530

    Intake screen is added.

--- app/src/main/java/com/kkwagh/kkw_poly/Departments.java
+++ app/src/main/java/com/kkwagh/kkw_poly/Departments.java
@@ -1,8 +1,11 @@
 package com.kkwagh.kkw_poly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class Departments extends AppCompatActivity {
 
@@ -10,5 +13,79 @@ public class Departments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_departments);
+
+
+        CardView card1, card2, card3, card4, card5, card6, card7, card8;
+
+        card1 = findViewById(R.id.card1);
+        card2 = findViewById(R.id.card2);
+        card3 = findViewById(R.id.card3);
+        card4 = findViewById(R.id.card4);
+        card5 = findViewById(R.id.card5);
+        card6 = findViewById(R.id.card6);
+        card7 = findViewById(R.id.card7);
+        card8 = findViewById(R.id.card8);
+
+
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Departments.this, Intake.class);
+                startActivity(intent);
+            }
+        });
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Departments.this, DeptCm.class));
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Departments.this, DeptCe.class);
+                startActivity(intent);
+            }
+        });
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Departments.this, DeptEe.class));
+            }
+        });
+
+        card5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Departments.this, DeptEntc.class);
+                startActivity(intent);
+            }
+        });
+
+        card6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Departments.this, DeptIt.class);
+                startActivity(intent);
+            }
+        });
+
+        card7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Departments.this, DeptCv.class);
+                startActivity(intent);
+            }
+        });
+
+        card8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Departments.this, DeptMe.class);
+                startActivity(intent);
+            }
+        });
     }
 }
