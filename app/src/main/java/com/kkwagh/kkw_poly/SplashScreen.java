078822dc6438f3223464a3612615d8f6d832be27
theCodeNilesh <61627365+theCodeNilesh@users.noreply.github.com>
2020-07-18 20:35:19 +0530

    Splash screen is changed

--- app/src/main/java/com/kkwagh/kkw_poly/SplashScreen.java
+++ app/src/main/java/com/kkwagh/kkw_poly/SplashScreen.java
@@ -5,20 +5,47 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 1000;
+    private static int SPLASH_SCREEN = 1500;
     SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView txt1,txt2,txt3,txt4;
+
+        Animation splashanim;
+
+
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        txt1=(TextView)findViewById(R.id.txt1);
+        txt2=(TextView)findViewById(R.id.txt2);
+        txt3=(TextView)findViewById(R.id.txt3);
+        txt4=(TextView)findViewById(R.id.txt4);
+
+        // This commented code is for tasting dont messup with this
+
+        //splashanim = AnimationUtils.loadAnimation(this, R.anim.splashanim);
+        //top1.startAnimation(splashanim);
+
+        txt1.setAlpha((float) 0.0);
+        txt2.setAlpha((float) 0.0);
+        txt3.setAlpha((float) 0.0);
+        txt4.setAlpha((float) 0.0);
+
+        txt1.animate().alpha((float) 1.0).setDuration(400).setStartDelay(250);
+        txt2.animate().alpha((float) 1.0).setDuration(400).setStartDelay(325);
+        txt3.animate().alpha((float) 1.0).setDuration(400).setStartDelay(525);
+        txt4.animate().alpha((float) 1.0).setDuration(400).setStartDelay(725);
 
 
         new Handler().postDelayed(new Runnable() {
